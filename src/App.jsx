2ade66d545072422cc18f1c38c92fc111474fc78
Michellebagot <121902516+Michellebagot@users.noreply.github.com>
2024-04-11 13:03:21 +0100

    addressed font issue with comfortaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Home from './components/Home/Home'
 import { Routes, Route } from 'react-router-dom'
+import React from 'react'
 import AboutMe from './components/AboutMe/AboutMe'
+import NavBar from './components/NavigationBar/NavigationBar'
 
 function App() {
 
   return (
     <>
+<NavBar />
+
 <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about-me" element={<AboutMe />} />
