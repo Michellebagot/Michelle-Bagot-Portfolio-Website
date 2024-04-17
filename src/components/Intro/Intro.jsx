9a65efc8ffe15af7b8fd94614df2f1c3d48f30f3
Michellebagot <121902516+Michellebagot@users.noreply.github.com>
2024-04-17 14:22:41 +0100

    stashed changes

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,16 +1,26 @@
-import CTA from "../CTA/CTA"
-import './intro.css'
+import CTA from "../CTA/CTA";
+import "./intro.css";
 
 const Intro = () => {
+  return (
+    <>
+      <div className="cardContainer">
+        <div className="headshotDiv">
+          <img
+            src="./assets/MB-headshot.jpg"
+            alt="Michelle Bagot Headshot"
+            className="headshotImg"
+          />
+        </div>
+        <div className="introTextDiv">
+            <h2>Hey!</h2>
+          <p className="inlineParagraph">I'm Michelle, {<CTA />}</p>
+          <p>Explore my projects to see how I develop software solutions that streamline workflows and empower users.</p>
+          <p>I'd love to work with you on your next project, so get in touch and lets chat about how I can help you create something amazing!</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return(
-        <>
-        <h2>Hey!</h2>
-        <p className="inlineParagraph">I'm Michelle, {<CTA />}</p>
-        
-        </>
-    )
-
-}
-
-export default Intro
+export default Intro;
