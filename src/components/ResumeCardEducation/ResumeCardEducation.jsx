596c5c454fcf51bbb76c35a00e34ce6cd2ca3aea
Michellebagot <121902516+Michellebagot@users.noreply.github.com>
2024-05-17 14:02:06 +0300

    pre job search commit

--- src/components/ResumeCardEducation/ResumeCardEducation.jsx
+++ src/components/ResumeCardEducation/ResumeCardEducation.jsx
@@ -2,7 +2,6 @@ import './resumeCardEducation.css'
 
 const ResumeCardEducation = ({school}) => {
 
-console.log(school)
     return(
         <section className='cardContainer'>
         <h3>{school.institution} - {school.subject}</h3>
