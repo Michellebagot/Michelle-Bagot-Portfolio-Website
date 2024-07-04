270c4f03b1520a2ef543bcfbb7d2aabd16790574
Michellebagot <121902516+Michellebagot@users.noreply.github.com>
2024-07-04 18:26:37 +0100

    refactor of code for formatting neatness

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-  <ScrollToTop />
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
